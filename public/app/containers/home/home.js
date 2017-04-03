0e6e75eedfda8a1f311b2aaff84321d58f498b0b
Tommy-Lim <tlim24@gmail.com>
2017-04-02 23:27:27 -0700

    added active selection for word

--- public/app/containers/home/home.js
+++ public/app/containers/home/home.js
@@ -9,8 +9,10 @@ function HomeCompCtrl(WordsService){
   var homeComp = this;
   homeComp.query = "";
   homeComp.results = [];
+  homeComp.currentResult = 0;
 
   homeComp.getWords = function(query){
+    homeComp.currentResult = 0;
     var queryArray = query.split(" ");
     var lastWord = queryArray[queryArray.length - 1];
     console.log(queryArray, lastWord)
@@ -21,8 +23,18 @@ function HomeCompCtrl(WordsService){
   }
 
   homeComp.clickKey = function(key){
-    if(key == 1 || key == '#'){
+    if(key == 1){
       // do nothing
+    } else if(key == '#'){
+      var resultLength = homeComp.results.length;
+      if(resultLength > 1){
+        // homeComp.results.push(homeComp.results.splice(0,1)[0]);
+        if(homeComp.currentResult < resultLength - 1){
+          homeComp.currentResult++;
+        } else{
+          homeComp.currentResult = 0;
+        }
+      }
     } else if (key == '*'){
       var queryLength = homeComp.query.length;
       if(queryLength > 0){
@@ -33,12 +45,21 @@ function HomeCompCtrl(WordsService){
       }
     } else if(key == 0){
       homeComp.query += " ";
-      homeComp.getWords(homeComp.query);
+      homeComp.currentResult = 0;
+      // homeComp.getWords(homeComp.query);
+      homeComp.results = [];
     } else{
       homeComp.query += (key + "");
       homeComp.getWords(homeComp.query);
     }
   }
+
+  homeComp.getLastWord = function(str){
+    var queryArray = str.split(" ");
+    var lastWord = queryArray[queryArray.length - 1];
+    return lastWord;
+  }
+
 }
 
 HomeCompCtrl.$inject = ['WordsService'];
