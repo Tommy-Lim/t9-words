2b095261da37f2a92ef8da004ad5f87c7c5cee52
Tommy-Lim <tlim24@gmail.com>
2017-04-03 09:36:18 -0700

    removed ability to add additional whitepsace

--- public/app/containers/home/home.js
+++ public/app/containers/home/home.js
@@ -16,11 +16,8 @@ function HomeCompCtrl($timeout, WordsService){
     homeComp.currentResult = 0;
     var queryArray = query.split(" ");
     var lastWord = queryArray[queryArray.length - 1];
-    console.log("nums", queryArray)
-    console.log("lastnum", lastWord);
     WordsService.getWords(lastWord).then(function(data){
       homeComp.results = data;
-      console.log("results", homeComp.results)
     });
   }
 
@@ -45,9 +42,7 @@ function HomeCompCtrl($timeout, WordsService){
       }
     } else if (key == 'Ast'){
       var queryLength = homeComp.query.length;
-      console.log("deleting from ",homeComp.query)
       if(queryLength > 0){
-        console.log("query length", queryLength);
         homeComp.query = homeComp.query.slice(0, queryLength - 1);
         homeComp.message = homeComp.message.slice(0, queryLength - 1);
         homeComp.getWords(homeComp.query);
@@ -55,27 +50,19 @@ function HomeCompCtrl($timeout, WordsService){
         // do nothing
       }
     } else if(key == '0'){
-      console.log("space hit")
-      console.log("Message:"+homeComp.message+"||")
       var messageLength = homeComp.message.length;
-      console.log("message length:", messageLength);
-
       var lastChar = homeComp.message.charAt(messageLength);
-      console.log("last char is |"+lastChar+"|");
       if(lastChar != " " && homeComp.message.length>0){
-        console.log("last char not space, so substring")
         var lastSpace = homeComp.message.lastIndexOf(" ");
-        console.log("last space index", lastSpace);
         var subMessage = homeComp.message.substring(0,lastSpace + 1);
-        console.log(subMessage);
         homeComp.message = subMessage;
       }
-      homeComp.query += " ";
+      if(homeComp.query.charAt(homeComp.query.length-1) != " "){
+        homeComp.query += " ";
+      }
       var word = homeComp.results[homeComp.currentResult]
       if(word){
         homeComp.message += (word + " ");
-      } else {
-        homeComp.message += " ";
       }
       homeComp.results = [];
     } else{
