8083199e169c1003d47d4bd03cdc3f8f507c7e09
Tommy-Lim <tlim24@gmail.com>
2017-04-03 16:21:05 -0700

    added /api/words/all route

--- controllers/words.js
+++ controllers/words.js
@@ -2,7 +2,17 @@ var express = require('express');
 var router = express.Router();
 var models = require('../models/schemas');
 var mongoose = require('mongoose');
+var WordsFile = require('../scripts/words.js')
 
+// FOR USE WITH FRONT END DICT BUILDER
+router.route('/all')
+.get(function(req, res){
+  res.send({
+    words: WordsFile.words
+  })
+})
+
+// For use with DB
 router.route('/:id')
 .get(function(req, res){
   console.log("ID", req.params.id);
